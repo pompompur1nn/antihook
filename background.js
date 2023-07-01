33b2c4628152a12bbfa833f029d7e5241d34fc25
logan <113845723+pompompur1nn@users.noreply.github.com>
2023-06-30 22:14:27 -0400

    added cisco, zulip, rocketchat, and twist

--- background.js
+++ background.js
@@ -57,7 +57,11 @@ browser.webRequest.onBeforeRequest.addListener(
     'https://media.guilded.gg/webhooks/*',
     'https://*.webhook.office.com/*',
     'https://api.flock.com/hooks/*',
-    'https://*.cloud.mattermost.com/hooks/*'
+    'https://*.cloud.mattermost.com/hooks/*',
+    'https://*.rocket.chat/hooks/*',
+    'https://*.zulipchat.com/api/v1/external/zuliprcbot/api/v1/*',
+    'https://api.ciscospark.com/v1/webhooks/incoming/*',
+    'https://api.twist.com/api/v3/integrations/webhooks/*/messages
   ],
   []
 );
